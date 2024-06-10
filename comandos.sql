ec49fde13c32f921b1316d2e9d65add167764e01
Fuzetto-pixel <mayconfuzetto@outlook.com>
2024-06-10 02:10:52 -0300

    wip

--- comandos.sql
+++ comandos.sql
@@ -1,6 +1,6 @@
 -- Comando para criar a tabela aluno
 CREATE TABLE aluno (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT PRIMARY KEY,
     nome VARCHAR(50),
     idade INT
 );
@@ -15,22 +15,19 @@ INSERT INTO aluno (nome, idade) VALUES
 
 -- # Comando para executar o banco de dados MySQL usando Docker:
 
--- docker run -d -p 3306:3306 --name atividade04-db-1 \
--- -e MYSQL_ROOT_PASSWORD=root \
--- -e MYSQL_USER=user \
--- -e MYSQL_PASSWORD=passwd \
--- -e MYSQL_DATABASE=db_aula \
--- mysql:latest
+-- docker run -d -p 3306:3306 --name atividade04-db-1 
+
 ---------------------------------------------------------------------------------------
 -- Comando para executar o contêiner da sua aplicação Node.js:
 
--- docker run -d -p 3000:3000 --name atividade04-app-1 atividade04-app
+-- docker run -d -p 3000:3000 --name atividade04-app-1 atividade04-app:latest
+
 -- Abra a URL http://localhost:3000/consulta-dados 
 
 ---------------------------------------------------------------------------------------
 -- links do github e dockerhub
 
-https://hub.docker.com/repository/docker/fuzetto/atividade04/general
+https://hub.docker.com/repository/docker/fuzetto/atividade04-app/general
 
 https://github.com/fuzetto-pixel/Atividade04
 
